837150c2bd6a354b57d0a9f1b7d18964075f35aa
LaviniaPop31 <laviniapop31@gmail.com>
2021-01-27 19:55:47 +0200

    implemented film roll

---
+++ pages/home/home.js
@@ -0,0 +1,15 @@
+// 1. Menu button
+//      - sa adaug funtionalitate pe click
+//      - sa apara pop-up cu optiunile din meniu
+//      - sa implementez paginile corespunzatoare
+// 2. Home button
+//      - sa ii adaug fuctionalitate pe click
+// 3. Search 
+//      - sa trimit la pagina de search
+// 4. Log in
+//      - sa trimit la pagina de log in
+// 5. Film roll
+//      - sa implementez film roll
+//      - sa afisez filmele in interiorul lui
+//      - sa implementez butoanele de previous movie/next movie, sa le fac sa dispara cand nu mai e film
+//      - sa adaug functionalitate filmelor, pe click sa afiseze in partea de jos descrierea 
