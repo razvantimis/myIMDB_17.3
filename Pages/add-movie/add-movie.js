4c596da347e613234848caa3456c02af8ce375b3
Rebee27 <gogrebeca12@gmail.com>
2021-01-27 19:55:50 +0200

    add

--- Pages/add-movie/add-movie.js
+++ Pages/add-movie/add-movie.js
@@ -1,43 +1,38 @@
 //API info
 const baseURL = "...";
 
-//add movie function
 
-function addMovie(post) {
-    //create a movie list
-    var movieList =...
-
-    //create a movie 
-    var movie =...
-
-        //add the details of the movie
-        movie.innerHTML = post.title + post.description .....
-
-        //append the movie to the movie list
-        movieList.appendChild(movie);
+function getInputsValues(titleInput, yearInput, typeInput, posterInput) {
+    var titleInput = getElementById(titleInput).value;
+    var yearInput = getElementById(yearInput).value;
+    var typeInput = getElementByClass(typeInput).value;
+    var posterInput = getElementById(posterInput).value;
 }
 
-//fetch the movie
-fetch(baseURL + "/movies", { method: "POST" })
-    .then(
-        function (response) {
-            // based on the Resonse status decide what to do
-            if (response.status === 200) {
-                return response.json();
-                // It returns a promise that resolves with the result of parsing the body text as JSON.
-                // The json() method takes a Response stream and reads it to completion.
-            } else if (response.status === 403) {
-                throw new Error("message: "'You need to be authenticated to be able to create a movie'");
-        }
-        }
 
-    )
-    .then(
-        function (jsonResp) {
-            // call the display function
-            addMovie(jsonResp);
-        }
-    ).catch(function (jsonResp) {
-        console.log(jsonResp);
-    });
-    )
+function addMovie(titleInput, yearInput, typeInput, posterInput) {
+    fetch(baseURL + "/movies", { method: "POST" })
+        .then(
+            function (response) {
+                if (response.status === 200) {
+                    console.log(response.json());
+                    return response.json();
+                } else if (response.status === 403) {
+                    throw new Error("message: 'You need to be authenticated to be able to create a movie'");
+                }
+            }
+
+        )
+        .then(
+            function (jsonResp) {
+                console.log(jsonResp)
+            }
+
+        )
+}
+
+// document.getElementById("done-button").addEventListener("click", displayMovie);
+
+// function displayMovie() {
+//     console.log(jsonResp);
+// }
