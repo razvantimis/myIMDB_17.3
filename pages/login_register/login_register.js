31f0529ab150a3d0d2be98edeed65c24bfb2a4a1
Razvan Timis <timisrazvanvasile@gmail.com>
2021-02-03 21:44:37 +0200

    Add login

--- pages/login_register/login_register.js
+++ pages/login_register/login_register.js
@@ -1,87 +1,88 @@
-const baseUrl = "https://movies-app-siit.herokuapp.com"; 
-const loginPath ="/auth/login";
-const registerPath="/auth/register";
+const baseUrl = "https://movies-app-siit.herokuapp.com";
+const loginPath = "/auth/login";
+const registerPath = "/auth/register";
 const errorMessage = document.getElementById("errorMessage");
 
 
 
 function parseResponse(response) {
-    if (response.status === 200) {
-        errorMessage.innerHtml="";
-        return response.json();
-    } else {
-        throw new Error("");
-        
-    }
+  if (response.status === 200) {
+    errorMessage.innerHtml = "";
+    return response.json();
+  } else {
+    throw new Error("");
+
+  }
 }
 
-function navigateToHomePage(){
-    window.location.href = "/pages/home/home.html";
+function navigateToHomePage() {
+  window.location.href = "/pages/home/home.html";
 }
 
- 
-function accountData (usernameParam,passwordParam) {
-    var username = document.getElementById(usernameParam);
-    var password = document.getElementById(passwordParam);
-    var object = {
-        username : username ,
-        password : password ,
-    }
+
+function accountData(usernameParam, passwordParam) {
+  var username = document.getElementById(usernameParam);
+  var password = document.getElementById(passwordParam);
+  var object = {
+    username: username,
+    password: password,
+  }
   return object;
-} 
+}
 
 
 
 
- 
- 
- var createAccountButton = document.getElementById('createAccountButton');
- var loginButton = document.getElementById('loginButton');
 
 
-loginButton.addEventListener("click", function(){
-    var responseData = accountData("username", "password");
-   
-    let loginData = {
-        username: responseData.username.value ,
-        password: responseData.password.value ,
-    }
-  
-    console.log("login data: " , loginData);
+var createAccountButton = document.getElementById('createAccountButton');
+var loginButton = document.getElementById('loginButton');
 
-    fetch(baseUrl + loginPath , { method: "POST" ,headers: { 
-        'Content-Type': 'application/json'
-     }, body: JSON.stringify(loginData) })
-        .then(parseResponse)
-        .then((response) => {
-            navigateToHomePage();
-            localStorage.setItem("token" , response.accessToken);
-            
-         })
-        .catch(error => { 
-            console.log("login error: ", errorMessage);
-            
-            errorMessage.innerHTML = "user not found/wrong password";
-            
-        }) 
 
-      
+loginButton.addEventListener("click", function () {
+  var responseData = accountData("username", "password");
+
+  let loginData = {
+    username: responseData.username.value,
+    password: responseData.password.value,
+  }
+
+  console.log("login data: ", loginData);
+
+  fetch(baseUrl + loginPath, {
+    method: "POST", headers: {
+      'Content-Type': 'application/json'
+    }, body: JSON.stringify(loginData)
+  })
+    .then(parseResponse)
+    .then((response) => {
+      navigateToHomePage();
+      localStorage.setItem("token", response.accessToken);
+
+    })
+    .catch(error => {
+      console.log("login error: ", errorMessage);
+
+      errorMessage.innerHTML = "user not found/wrong password";
+
+    })
+
+
 });
 
-createAccountButton.addEventListener("click", function(){
-    var responseData = accountData("createUsername", "createPassword");
-    let registerData = {
-        username: responseData.username.value ,
-        password: responseData.password.value ,
-    }
+createAccountButton.addEventListener("click", function () {
+  var responseData = accountData("createUsername", "createPassword");
+  let registerData = {
+    username: responseData.username.value,
+    password: responseData.password.value,
+  }
 
-    fetch(baseUrl + registerPath , { method: "POST" , headers:{'Content-Type': 'application/json', }, body:JSON.stringify(registerData)  })
-        .then(parseResponse)
-        .then(() => {
-           navigateToHomePage();
-          })
-        .catch(error => {errorMessage.innerHTML = "Username already existing"});
+  fetch(baseUrl + registerPath, { method: "POST", headers: { 'Content-Type': 'application/json', }, body: JSON.stringify(registerData) })
+    .then(parseResponse)
+    .then(() => {
+      navigateToHomePage();
+    })
+    .catch(error => { errorMessage.innerHTML = "Username already existing" });
 
-        
+
 });
-
