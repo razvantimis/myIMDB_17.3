2a074a09cf8f1f7b8c487d3bb756c48101e3ea7f
Vladimir Cimpeanu <vladimir.cimpeanu@yahoo.com>
2021-02-05 17:04:51 +0100

    added log in and log out buttons to header

--- pages/login_register/login_register.js
+++ pages/login_register/login_register.js
@@ -1,14 +1,47 @@
 const baseUrl = "https://movies-app-siit.herokuapp.com";
 const loginPath = "/auth/login";
+const logOutPath = "/auth/logout"
 const registerPath = "/auth/register";
 const errorMessage = document.getElementById("errorMessage");
 
+function signIn(){
+  const buttons = document.getElementById("buttonContainer");
+ 
+ 
+  
+  
+  
+  
+  
+  if (localStorage.getItem("token")){
+    
+    var logOutBtn = document.createElement("button");
+    logOutBtn.classList.add("headerButtons");
+    logOutBtn.setAttribute("id","logOutButton");
+  logOutBtn.textContent = "Log Out";
+  buttons.appendChild(logOutBtn);
+  } else {
+    var loginBtn = document.createElement("button");
+    loginBtn.classList.add("headerButtons");
+    loginBtn.setAttribute("id", "loginButton");
+    loginBtn.textContent = "Login";
+    buttons.appendChild(loginBtn);
+  }
+
+
+console.log("localtoken:" , localStorage.getItem("token"))
+
+
+}
+
+signIn()
 
 
 function parseResponse(response) {
   if (response.status === 200) {
     errorMessage.innerHtml = "";
     return response.json();
+  
   } else {
     throw new Error("");
 
@@ -19,10 +52,14 @@ function navigateToHomePage() {
   window.location.href = "/pages/home/home.html";
 }
 
+function navigateToLoginPage(){
+  window.location.href = "/pages/login_register/login_register.html"
+}
 
 function accountData(usernameParam, passwordParam) {
-  var username = document.getElementById(usernameParam);
-  var password = document.getElementById(passwordParam);
+  var username = document.getElementById(usernameParam).value;
+  var password = document.getElementById(passwordParam).value;
+  console.log("username:" , username);
   var object = {
     username: username,
     password: password,
@@ -36,48 +73,60 @@ function accountData(usernameParam, passwordParam) {
 
 
 var createAccountButton = document.getElementById('createAccountButton');
+
+
 var loginButton = document.getElementById('loginButton');
+
 
 
 loginButton.addEventListener("click", function () {
   var responseData = accountData("username", "password");
-
-  let loginData = {
-    username: responseData.username.value,
-    password: responseData.password.value,
-  }
-
-  console.log("login data: ", loginData);
-
+  
   fetch(baseUrl + loginPath, {
     method: "POST", headers: {
       'Content-Type': 'application/json'
-    }, body: JSON.stringify(loginData)
+    }, body: JSON.stringify(responseData)
   })
     .then(parseResponse)
     .then((response) => {
       navigateToHomePage();
       localStorage.setItem("token", response.accessToken);
-
-    })
+})
     .catch(error => {
       console.log("login error: ", errorMessage);
-
       errorMessage.innerHTML = "user not found/wrong password";
+})
+});
 
-    })
+var logOutButton = document.getElementById('logOutButton');
 
+ logOutButton.addEventListener("click", function () {
+ let token = localStorage.getItem("token");
+  fetch(baseUrl+logOutPath, 
+     { method: "GET" , 
+       headers:{ 
+       "X-Auth-Token": token
+       }
+     })
+     .then(parseResponse)
+     .then((response) => {
+     window.localStorage.removeItem("token");
+     })
+     .catch(error => {
+      
+    console.error(error)
 
+    errorMessage.innerHTML = "You have to be logged-in in order to log out";
+     })
+  
 });
 
 createAccountButton.addEventListener("click", function () {
   var responseData = accountData("createUsername", "createPassword");
-  let registerData = {
-    username: responseData.username.value,
-    password: responseData.password.value,
-  }
+  
 
-  fetch(baseUrl + registerPath, { method: "POST", headers: { 'Content-Type': 'application/json', }, body: JSON.stringify(registerData) })
+  fetch(baseUrl + registerPath, { method: "POST", headers: { 'Content-Type': 'application/json', },
+   body: JSON.stringify(responseData) })
     .then(parseResponse)
     .then(() => {
       navigateToHomePage();
@@ -86,3 +135,4 @@ createAccountButton.addEventListener("click", function () {
 
 
 });
+
