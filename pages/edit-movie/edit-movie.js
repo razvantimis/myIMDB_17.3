d03979f3aa73c8981d1446cf71666453b4ca4754
Rebee27 <gogrebeca12@gmail.com>
2021-02-10 17:43:13 +0200

    finishing add/edit movie pages

--- pages/edit-movie/edit-movie.js
+++ pages/edit-movie/edit-movie.js
@@ -1,6 +1,6 @@
-baseUrl = "https://movies-app-siit.herokuapp.com/movies";
+baseUrl = "https://movies-app-siit.herokuapp.com/movies/";
 
-
+let titleInput = document.getElementById("titleInput");
 let yearInput = document.getElementById("yearInput");
 let ratingInput = document.getElementById("ratingInput");
 let dateInput = document.getElementById("dateInput");
@@ -13,6 +13,7 @@ let plotInput = document.getElementById("plotInput");
 let languageInput = document.getElementById("languageInput");
 let countryInput = document.getElementById("countryInput");
 let posterInput = document.getElementById("posterInput");
+let movieId = location.search.substring(4);
 
 
 //go to home page
@@ -21,13 +22,7 @@ function goToHome() {
 }
 
 
-function getValues() {
-
-}
-
-const editMovie = () => {
-
-    let loginToken = localStorage.getItem("token");
+const getInputs = () => {
 
     var data = {
         Title: titleInput.value,
@@ -46,22 +41,39 @@ const editMovie = () => {
     };
 
 
-    fetch("https://movies-app-siit.herokuapp.com/movies/:id", {
+    fetch(baseUrl + movieId, {
         method: "GET",
-        body: JSON.stringify(data),
-        headers: {
-            "X-Auth-Token": loginToken,
-            "Content-Type": "application/json"
-        }
-            .then((response) => response.json())
-            .then((json) => {
-                console.log(json);
-                getValues();
-            })
-            .catch((err) => console.log(err))
     })
+        .then((response) => response.json())
+        .then((json) => {
+            console.log(json);
+            const movie = json;
+            titleInput.value = json.Title;
+            yearInput.value = json.Year;
+            ratingInput.value = json.Rating;
+            dateInput.value = json.Date;
+            timeInput.value = json.Time;
+            genreInput.value = json.Genre;
+            directorInput.value = json.Director;
+            writerInput.value = json.Writer;
+            actorsInput.value = json.Actors;
+            plotInput.value = json.Plot;
+            languageInput.value = json.Language;
+            countryInput.value = json.Country;
+            posterInput.value = json.Poster;
 
 
+
+        })
+        .catch((err) => console.log(err))
+}
+
+
+getInputs();
+
+const editMovie = () => {
+
+    let loginToken = localStorage.getItem("token");
 
     fetch("https://movies-app-siit.herokuapp.com/movies/:id", {
         method: "PUT",
@@ -81,7 +93,6 @@ const editMovie = () => {
 
 }
 
-
 var doneButton = document.getElementById("done-button");
 doneButton.addEventListener("click", editMovie);
 
@@ -93,90 +104,12 @@ doneButton.addEventListener("click", editMovie);
 
 
 //HEADER
-displayMenu()
-// added functionality to menu button
-function displayMenu() {
-    let menuButton = document.getElementById('menuIcon');
-    let menuContainer = document.getElementById('menuContainer');
-    menuButton.addEventListener('click', function () {
-        menuContainer.style.display = 'block';
-        displayActionMovies();
-        displayDramaMovies();
-        displayAnimationMovies();
-        displaySciFiMovies();
-        displayFamilyMovies();
-
-    })
-}
-
-function displayActionMovies() {
-    let actionButton = document.getElementById('actionMovies');
-    actionButton.addEventListener('click', function () {
-
-        getMovieCategory('action')
-        hideShow()
-
-        let title = document.getElementById('moviesCategoryTitle');
-        title.innerText = 'Action'
-    })
-}
-
-function displayDramaMovies() {
-    let dramaButton = document.getElementById('dramaMovies');
-    dramaButton.addEventListener('click', function () {
-
-        getMovieCategory('drama')
-        hideShow()
-
-        let title = document.getElementById('moviesCategoryTitle');
-        title.innerText = 'Drama'
-    })
-
-}
-
-function displayAnimationMovies() {
-    let animationButton = document.getElementById('animationMovies');
-    animationButton.addEventListener('click', function () {
-
-        getMovieCategory('animation')
-        hideShow()
-
-        let title = document.getElementById('moviesCategoryTitle');
-        title.innerText = 'Animation'
-    })
-}
-
-function displaySciFiMovies() {
-    let sciFiButton = document.getElementById('sciFiMovies');
-    sciFiButton.addEventListener('click', function () {
-
-        getMovieCategory('sci-fi')
-        hideShow()
-
-        let title = document.getElementById('moviesCategoryTitle');
-        title.innerText = 'Sci-Fi'
-    })
-}
-
-function displayFamilyMovies() {
-    let familyButton = document.getElementById('familyMovies');
-    familyButton.addEventListener('click', function () {
-
-        getMovieCategory('family');
-        hideShow()
-
-        let title = document.getElementById('moviesCategoryTitle');
-        title.innerText = 'Family'
-    })
-}
-
-
 //go to Home page
 displayHome()
 function displayHome() {
     let homeButton = document.getElementById('home');
     homeButton.addEventListener('click', () => {
-        window.location.href = '/Pages/home/home.html';
+        window.location.href = '/pages/home/home.html';
     })
 }
 
@@ -189,19 +122,6 @@ function displayLogin() {
     })
 }
 
-
-// hiding menuContainer when the user clicks outside of the container
-hideMenuContainer()
-function hideMenuContainer() {
-    document.onclick = function (event) {
-        let menuIcon = document.getElementById('menuIcon');
-        if (event.target !== menuIcon) {
-            menuContainer.style.display = 'none';
-        }
-    }
-}
-
-
 setPointer();
 function setPointer() {
     document.getElementById("menuButton").style.cursor = "pointer";
@@ -211,4 +131,4 @@ function setPointer() {
     document.getElementById("login").style.cursor = "pointer";
     document.getElementById("searchBar").style.cursor = "pointer";
     document.getElementById("done-button").style.cursor = "pointer";
-}
+} 
